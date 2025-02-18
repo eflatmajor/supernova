3ed2e45330f231871fced06bbcdc8c170b90fed0
eflatmajor <bachbeethovenundbrahms@gmail.com>
2025-02-18 02:18:22 +0000

    Add randomSequence()

--- src/utilities/array.js
+++ src/utilities/array.js
@@ -1,3 +1,40 @@
-export function randomElement(array) {
+function randomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
+
+function randomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+const sequences = {};
+
+function randomSequence(id, elements) {
+  if ( ! sequences[id]) {
+    sequences[id] = { elements: [...elements] };
+  }
+
+  let sequence = sequences[id];
+
+  if (sequence.elements.length === elements.length) {
+    /*
+      Introduces a pattern in the randomness (is this really an issue?),
+      but successfully prevents repetition across the boundaries of each
+      run through the sequence.
+    */
+    return sequence.elements.splice(0, 1)[0];
+  }
+  else {
+    let index = randomIndex(sequence.elements);
+    let value = sequence.elements[index];
+
+    sequence.elements.splice(index, 1);
+
+    if (sequence.elements.length === 0) {
+      sequence.elements = [...elements];
+    }
+
+    return value;
+  }
+}
+
+export { randomElement, randomIndex, randomSequence }
