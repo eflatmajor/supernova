1092439e4cb175dc908781934fafb48b9a85d946
eflatmajor <bachbeethovenundbrahms@gmail.com>
2025-02-18 02:20:12 +0000

    Remove stray file.

--- src/game/statistics.js
+++
@@ -1,9 +0,0 @@
-/*
-  Player statistics.
-
-  Track various things so we can in future provide a nice cool statistics screen.
-*/
-
-export const STATISTICS = {
-  MINUTES_PLAYED: "minutesPlayed"
-};
