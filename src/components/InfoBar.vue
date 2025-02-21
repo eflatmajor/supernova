ec94616416baa25ddb44277289653515671951f0
eflatmajor <bachbeethovenundbrahms@gmail.com>
2025-02-21 00:51:46 +0000

    UI experiment - Add InfoBar & Overlay components.

---
+++ src/components/InfoBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="info_bar">
+    <nav>
+      <ul>
+        <li>❤️ <sub>x</sub><span title="Current Hitpoints">{{ health }}</span></li>
+        <li>🛡️ <sub>x</sub><span title="Shield Capacity">{{ shields }}</span></li>
+        <li>💰 <sub>x</sub><span title="Credits Owned">{{ money }}</span></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+// TODO: Fetch player data from the state store.
+
+export default {
+  data() {
+    return {
+      health: 10,
+      shields: 42,
+      money: 69
+    };
+  }
+};
+</script>
+
+<style scoped>
+div#info_bar {
+  position: fixed;
+  width: 100%;
+  z-index: 420;
+  color: white;
+}
+
+ul {
+  padding: 0.25rem 0;
+  height: 100%;
+  display: flex;
+  justify-content:space-around;
+  align-items: center;
+  list-style-type: none;
+  /* background-color: var(--purple-4); */
+  background-color: black;
+  opacity: 0.7;
+}
+</style>
---
+++ src/components/Overlay.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="overlay" :style="styles" :class="classes">
+    <slot>
+      <h1>I Am A Full-Screen Overlay</h1>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["opacity", "center"],
+
+  data() {
+    return {
+      _center: this.center ?? true,
+      _opacity: this.opacity ?? 0.75
+    };
+  },
+
+  computed: {
+    styles() {
+      return {
+        opacity: this._opacity
+      };
+    },
+    classes() {
+      return {
+        center: this._center
+      }
+    }
+  },
+}
+</script>
+
+<style>
+div.overlay {
+  font-family: monospace;
+  height: calc(100vh - 32px);
+  width: 100vw;
+  padding: 1em;
+  background-color: black;
+  color: white;
+  position: absolute;
+}
+div.overlay.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
